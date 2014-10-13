76a55057d649d3f66a22c3244d2ab7aa2efff1bd
Bertrand Le Roy <bertrandleroy@gmail.com>
2014-10-13 11:12:44 -0700

    Fixed file inclusions in localization module.

--- modules/core/localization/index.js
+++ modules/core/localization/index.js
@@ -1,6 +1,6 @@
 // DecentCMS (c) 2014 Bertrand Le Roy, under MIT license. See license.txt for licensing details.
 'use strict';
-var t = require('lib/t');
+var t = require('./lib/t');
 
 module.exports = {
   t: t
