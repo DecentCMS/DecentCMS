ef14c56b80dca56e30f23ac4e483486cc6ef4e2d
Bertrand Le Roy <bertrandleroy@gmail.com>
2014-10-18 13:34:16 -0700

    Token tests don't require a dependency on multi-tenancy

--- modules/core/tokens/test/index.js
+++ modules/core/tokens/test/index.js
@@ -1,37 +1,33 @@
 // DecentCMS (c) 2014 Bertrand Le Roy, under MIT. See LICENSE.txt for licensing details.
 'use strict';
 var expect = require('chai').expect;
-
-var Shell = require('../../multi-tenancy/lib/shell');
-var shell = new Shell({
-  services: {
-    token: [require('../lib/token')]
-  }
-});
+var EventEmitter = require('events').EventEmitter;
+var shell = new EventEmitter();
+var Token = require('../lib/token');
 
 describe('Token', function() {
   it('will leave literal strings alone', function() {
     var str = 'Hi, I\'m a string.';
-    var token = shell.require('token');
+    var token = new Token(shell);
     expect(token.interpolate(str, {foo:42}))
       .to.equal(str);
   });
 
   it('will evaluate tokens', function() {
     var str = 'A string with tokens in it: {{foo}} and {{bar}}.';
-    var token = shell.require('token');
+    var token = new Token(shell);
     expect(token.interpolate(str, {foo:42, bar: 'baz'}))
       .to.equal('A string with tokens in it: 42 and baz.');
   });
 
   it('will use pipes in tokens', function() {
     var str = 'A string with a piped token in it: {{foo|times>5}}.';
-    shell.on(shell.services.token[0].registerPipesEvent, function(pipes) {
+    shell.on(Token.registerPipesEvent, function(pipes) {
       pipes.times = function(str, n) {
         return new Array(parseInt(n || 1) + 1).join(str)
       }
     });
-    var token = shell.require('token');
+    var token = new Token(shell);
     expect(token.interpolate(str, {foo:42}))
       .to.equal('A string with a piped token in it: 4242424242.');
   });
