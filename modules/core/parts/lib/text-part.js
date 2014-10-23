b47e17786a7c964ac2e3c1c095cde31639a6c069
Bertrand Le Roy <bertrandleroy@gmail.com>
2014-10-22 19:33:05 -0700

    Text part with support for flavors.

--- modules/core/parts/lib/text-part.js
+++ modules/core/parts/lib/text-part.js
@@ -1,2 +1,42 @@
 // DecentCMS (c) 2014 Bertrand Le Roy, under MIT. See LICENSE.txt for licensing details.
 'use strict';
+var path = require('path');
+
+function TextPart(shell, options) {
+  this.shell = shell;
+  this.item = options.item;
+}
+
+TextPart.on = {
+  'decent.core.handle-item': function(shell, options) {
+    var content = options.shape;
+    if (!content.meta
+      || content.meta.type !== 'content'
+      || !content.temp
+      || !content.temp.item
+      || !content.temp.item.title) return;
+    var temp = content.temp;
+    var item = temp.item;
+    var renderer = options.renderStream;
+    var contentManager = renderer.contentManager;
+    var textParts = contentManager.getParts(item, 'text');
+    for (var i = 0; i < textParts.length; i++) {
+      var partName = textParts[i];
+      var part = item[partName];
+      var text = part.text;
+      if (!text && part._data) {
+        text = part._data;
+      }
+      var flavor = part.flavor
+        || (part.path ? path.extname(part.path).substr(1) : 'plain-text');
+      temp.shapes.push({
+        meta: {type: 'text', name: partName},
+        temp: {displayType: temp.displayType},
+        text: text,
+        flavor: flavor
+      });
+    }
+  }
+};
+
+module.exports = TextPart;
---
+++ modules/core/parts/views/text.js
@@ -0,0 +1,23 @@
+// DecentCMS (c) 2014 Bertrand Le Roy, under MIT. See LICENSE.txt for licensing details.
+'use strict';
+var html = require('htmlencode');
+
+module.exports = function textTemplate(textPart, renderer, shell) {
+  switch(textPart.flavor) {
+    case 'plain-text':
+      var html = html.htmlEncode(textPart.text).replace('\n', '<br/>\n');
+      renderer.write(html);
+      break;
+    case 'html':
+      renderer.write(textPart.text);
+      break;
+    default:
+      var flavorHandlers = shell.getServices('text-flavor');
+      for (var i = 0; i < flavorHandlers.length; i++) {
+        if (flavorHandlers[i].matches(textPart.flavor)) {
+          renderer.write(flavorHandlers[i].getHtml(textPart.text));
+          break;
+        }
+      }
+  }
+};
