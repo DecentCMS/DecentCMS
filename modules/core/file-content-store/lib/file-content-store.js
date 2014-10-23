dd8d20a590058782a941a0ea518a94501132fdea
Bertrand Le Roy <bertrandleroy@gmail.com>
2014-10-22 22:00:07 -0700

    Unwound some of the nested callback madness in the file content store using the async library.

--- modules/core/file-content-store/lib/file-content-store.js
+++ modules/core/file-content-store/lib/file-content-store.js
@@ -2,6 +2,7 @@
 'use strict';
 var path = require('path');
 var fs = require('fs');
+var async = require('async');
 
 /**
  * @description
@@ -22,75 +23,69 @@ FileContentStore.on = {
     var itemsToFetch = payload.itemsToFetch;
     var callback = payload.callback;
 
-    var count = 0;
     var siteDataRoot = path.join(shell.rootPath, 'data/content');
-    var handle = function(id, filePath, data) {
+
+    var handle = function handleITemData(id, filePath, data, callback) {
       // Parse the content item file
       var item = JSON.parse(data);
-      // Prepare the last operations for later.
-      var finalCallback = function() {
-        // Add the file path to temp meta data
-        var temp = shapeHelper.temp(item);
-        temp.filePath = filePath;
-        // Add to the items list
-        items[id] = item;
-        // Call all the item-specific callbacks
-        for (var j = 0; j < itemsToFetch[id].length; j++) {
-          itemsToFetch[id](null, item);
-        }
-        // Remove from the list of remaining items to fetch
-        delete itemsToFetch[id];
-        // If we're done, call the global callback
-        if (--count <= 0) {
-          callback();
-        }
-      };
       // Look for any part that needs to load an additional file.
       // Typically, that could be a markdown file for the body.
-      var count = 0;
-      for (var partName in item) {
-        if (partName === 'meta' || partName === 'temp') continue;
-        var part = item[partName];
-        if (part.hasOwnProperty('path')) {
-          count++;
-          var extraFilePath = path.join(path.dirname(filePath), part.path);
-          fs.readFile(extraFilePath, function(err, data) {
-            // TODO: handle error
-            part._data = data.toString();
-            count--;
-            if (count <= 0) finalCallback();
+      async.each(Object.keys(item), function lookForExtraFile(partName, next) {
+          var part;
+          if (partName !== 'meta'
+            && partName !== 'temp'
+            && (part = item[partName])
+            && part.hasOwnProperty('path')) {
+            var extraFilePath = path.join(path.dirname(filePath), part.path);
+            fs.readFile(extraFilePath, function extraFileRead(err, data) {
+              if (err) { next(err); return; }
+              part._data = data.toString();
+              next();
+            });
+          }
+          else {
+            next();
+          }
+        },
+        function allExtraFilesRead(err) {
+          if (err) { callback(err); return; }
+          // Add the file path to temp meta data
+          var temp = shapeHelper.temp(item);
+          temp.filePath = filePath;
+          // Add to the items list
+          items[id] = item;
+          // Call all the item-specific callbacks
+          if (Array.isArray(itemsToFetch[id])) {
+            for (var j = 0; j < itemsToFetch[id].length; j++) {
+              itemsToFetch[id](null, item);
+            }
+          }
+          // Remove from the list of remaining items to fetch
+          delete itemsToFetch[id];
+          callback();
+        });
+    };
+
+    async.each(Object.keys(itemsToFetch), function findItemFile(id, next) {
+      // TODO: support for redirected roots where a folder points at another. This will enable module documentation sub sites.
+      var itemFilePath = path.join(siteDataRoot, id, 'index.json');
+      fs.readFile(itemFilePath, function readIndexFile(err, data) {
+        if (!err) {
+          handle(id, itemFilePath, data.toString(), next);
+        }
+        else {
+          // File not found is normal, so let it flow, but re-throw others
+          if (err.code !== 'ENOENT') { next(err); return; }
+          // This was not a folder, maybe it was already a file.
+          itemFilePath = path.join(siteDataRoot, id);
+          fs.readFile(itemFilePath, function readItemFile(err, data) {
+            if (err) { next(err); return; }
+            handle(id, itemFilePath, data.toString(), next);
           });
         }
-      }
-      if (count <= 0) finalCallback();
-    };
-    for (var id in itemsToFetch) {
-      count++;
-      (function(id) {
-        // TODO: support for redirected roots where a folder points at another. This will enable module documentation sub sites.
-        var itemFilePath = path.join(siteDataRoot, id, 'index.json');
-        fs.readFile(itemFilePath, function(err, data) {
-          if (err) {
-            // File not found is normal, so let it flow, but re-throw others
-            if (err.code !== 'ENOENT') throw err;
-            // Not a folder, maybe a file
-            itemFilePath = path.join(siteDataRoot, id);
-            fs.readFile(itemFilePath, function(err, data) {
-              if (err) {
-                for(var j = 0; j < itemsToFetch[id].length; j++) {
-                  itemsToFetch[id](err);
-                }
-                count--;
-                return;
-              }
-              handle(id, itemFilePath, data.toString());
-            });
-            return;
-          }
-          handle(id, itemFilePath, data.toString());
-        });
-      })(id);
-    }
+      });
+    },
+    callback);
   }
 };
 
