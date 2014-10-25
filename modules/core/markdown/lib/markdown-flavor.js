2c916c30d57abf51f1f97ea1647862c78ee1f544
Bertrand Le Roy <bertrandleroy@gmail.com>
2014-10-25 00:15:40 -0700

    Markdown documentation

--- modules/core/markdown/lib/markdown-flavor.js
+++ modules/core/markdown/lib/markdown-flavor.js
@@ -1,16 +1,41 @@
 // DecentCMS (c) 2014 Bertrand Le Roy, under MIT. See LICENSE.txt for licensing details.
 'use strict';
 
+/**
+ * @description
+ * Markdown text field flavor
+ * @param shell
+ * @param options
+ * @param {boolean} [options.gfm] Enable GitHub-flavored Markdown.
+ * @param {boolean} [options.tables] Enable GFM tables.
+ * @param {boolean} [options.breaks] Enable GFM line breaks.
+ * @param {boolean} [options.pedantic] Conform to markdown.pl, bugs included.
+ * @param {boolean} [options.sanitize] Sanitize the output.
+ * @param {boolean} [options.smartLists] Smarter list behavior.
+ * @param {boolean} [options.smartyPants] Use smart typographic punctuation.
+ * @constructor
+ */
 function MarkdownFlavor(shell, options) {
   this.shell = shell;
   this.marked = require('marked');
   this.marked.setOptions(options);
 }
 
+/**
+ * Matches if the flavor is 'md' or 'markdown'.
+ * @param {string} flavor The flavor to match.
+ * @returns {boolean} true if the flavor is md or markdown.
+ */
 MarkdownFlavor.prototype.matches = function(flavor) {
   return flavor === 'markdown' || flavor === 'md';
 };
 
+/**
+ * @description
+ * Transforms the markdown code into HTML.
+ * @param {string} markdown The Markdown code.
+ * @returns {string} The HTML.
+ */
 MarkdownFlavor.prototype.getHtml = function(markdown) {
   return this.marked(markdown);
 };
