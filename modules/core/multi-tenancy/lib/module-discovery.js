97586695ec4617bb38c6c177e7ba81a1098b8592
Bertrand Le Roy <bertrandleroy@gmail.com>
2014-11-02 16:48:04 -0800

    Module discovery can find modules directly under the top modules folder (instead of in a module area), and by default looks under modules and themes top-level directories.

--- modules/core/multi-tenancy/lib/module-discovery.js
+++ modules/core/multi-tenancy/lib/module-discovery.js
@@ -9,64 +9,73 @@ var availableModules = {};
 /**
  * @description
  * Discovers modules under the paths passed as parameters. If no parameter is
- * passed in, the search is performed under /modules.
+ * passed in, the search is performed under /modules and /themes.
  */
 function discover() {
   Array.prototype.forEach.call(
-    (arguments.length === 0 ? ['./modules'] : arguments),
+    (arguments.length === 0 ? ['./modules', './themes'] : arguments),
     function(rootPath) {
       console.log(t('Discovering module areas in %s', rootPath));
       var areaPaths = fs.readdirSync(rootPath).map(function(subPath) {
         return path.resolve(rootPath, subPath);
       });
-      areaPaths.forEach(function (areaPath) {
-        console.log(t('Discovering modules in %s', areaPath));
-        var pathStats = fs.statSync(areaPath);
-        if (!pathStats.isFile()) {
-          var modulePaths = fs.readdirSync(areaPath).map(function(subPath) {
-            return path.join(areaPath, subPath);
-          });
-          modulePaths.forEach(function(modulePath) {
-            var manifestPath = path.join(modulePath, 'package.json');
-            if (!fs.existsSync(manifestPath)) return;
+      areaPaths.forEach(function discoverArea(areaPath) {
+        var areaManifestPath = path.join(areaPath, 'package.json');
+        var modulePaths;
+        if (fs.existsSync(areaManifestPath)) {
+          // This is a module, not a module area
+          modulePaths = [areaPath];
+        }
+        else {
+          console.log(t('Discovering modules in %s', areaPath));
+          var pathStats = fs.statSync(areaPath);
+          if (!pathStats.isFile()) {
+            modulePaths = fs.readdirSync(areaPath).map(function(subPath) {
+              return path.join(areaPath, subPath);
+            });
+          }
+        }
+        if (!modulePaths) return;
+        modulePaths.forEach(function discoverModule(modulePath) {
+          var manifestPath = path.join(modulePath, 'package.json');
+          if (!fs.existsSync(manifestPath)) return;
+          try {
+            var manifest = require(manifestPath);
+            var moduleName = manifest.name;
+            manifest.physicalPath = path.dirname(manifestPath);
+            console.log(t('Loaded module %s from %s', moduleName, manifestPath));
+            availableModules[moduleName] = manifest;
+            // Look for self-configuring services under /services
+            if (!manifest.services) manifest.services = {};
+            if (!manifest.features) manifest.features = {};
+            var servicesPath = path.join(modulePath, 'services');
             try {
-              var manifest = require(manifestPath);
-              var moduleName = manifest.name;
-              manifest.physicalPath = path.dirname(manifestPath);
-              console.log(t('Loaded module %s from %s', moduleName, manifestPath));
-              availableModules[moduleName] = manifest;
-              // Look for self-configuring services under /services
-              if (!manifest.services) manifest.services = {};
-              if (!manifest.features) manifest.features = {};
-              var servicesPath = path.join(modulePath, 'services');
-              try {
-                var servicePaths = fs.readdirSync(servicesPath);
-                for(var i = 0; i < servicePaths.length; i++) {
-                  var serviceFileName = servicePaths[i];
-                  if (path.extname(serviceFileName) === '.js') {
-                    var servicePath = path.join(servicesPath, serviceFileName).slice(0, -3);
-                    var service = require(servicePath);
-                    var serviceName = service.service || path.basename(serviceFileName, '.js');
-                    // TODO: build default feature from module name
-                    var serviceFeature = service.feature;
-                    var serviceDependencies = service.dependencies;
-                    manifest.services[serviceName] = {
-                      path: servicePath,
-                      feature: serviceFeature,
-                      dependencies: serviceDependencies
-                    };
-                  }
+              var servicePaths = fs.readdirSync(servicesPath);
+              for(var i = 0; i < servicePaths.length; i++) {
+                var serviceFileName = servicePaths[i];
+                if (path.extname(serviceFileName) === '.js') {
+                  var servicePath = path.join(servicesPath, serviceFileName).slice(0, -3);
+                  var service = require(servicePath);
+                  var serviceName = service.service || path.basename(serviceFileName, '.js');
+                  // TODO: build default feature from module name
+                  var serviceFeature = service.feature;
+                  var serviceDependencies = service.dependencies;
+                  manifest.services[serviceName] = {
+                    path: servicePath,
+                    feature: serviceFeature,
+                    dependencies: serviceDependencies
+                  };
                 }
               }
-              catch(dirEx) {}
             }
-            catch(ex) {
-              ex.path = manifestPath;
-              ex.message = t('Failed to load %s.', manifestPath);
-              throw ex;
-            }
-          });
-        }
+            catch(dirEx) {}
+          }
+          catch(ex) {
+            ex.path = manifestPath;
+            ex.message = t('Failed to load %s.', manifestPath);
+            throw ex;
+          }
+        });
       });
     });
 }
