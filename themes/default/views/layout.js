b0a39033f677dfb16cf9a281d3a3bf23c36a8a5b
Bertrand Le Roy <bertrandleroy@gmail.com>
2014-11-04 11:14:05 -0800

    Default layout supporting stylesheets.

--- themes/default/views/layout.js
+++ themes/default/views/layout.js
@@ -2,13 +2,17 @@
 'use strict';
 
 module.exports = function layoutTemplate(layout, renderer, shell) {
+  renderer.addMeta('generator', 'DecentCMS');
+  renderer.addStyleSheet('bootstrap');
+
   renderer.writeLine('<!DOCTYPE html>');
   renderer.writeLine('<html>');
   renderer.writeLine('<head>');
   renderer.write('  <title>');
   renderer.writeEncoded(renderer.title);
   renderer.writeLine('</title>');
-  // TODO: render scripts and stylesheets
+  renderer.renderMeta();
+  renderer.renderStyleSheets();
   renderer.writeLine('</head><body>');
   if (layout.main) {
     shell.emit('decent.core.shape.render', {
