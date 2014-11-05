357aec4f107ada00aa912dd9a0ef5375bf131580
Bertrand Le Roy <bertrandleroy@gmail.com>
2014-11-05 12:38:36 -0800

    Renaming shell to scope in layout template files.

--- themes/default/views/layout.js
+++ themes/default/views/layout.js
@@ -1,7 +1,7 @@
 // DecentCMS (c) 2014 Bertrand Le Roy, under MIT. See LICENSE.txt for licensing details.
 'use strict';
 
-module.exports = function layoutTemplate(layout, renderer, shell) {
+module.exports = function layoutTemplate(layout, renderer, scope) {
   renderer
     .addMeta('generator', 'DecentCMS')
     .addStyleSheet('bootstrap')
@@ -21,7 +21,7 @@ module.exports = function layoutTemplate(layout, renderer, shell) {
     .startTag('body');
 
   if (layout.main) {
-    shell.emit('decent.core.shape.render', {
+    scope.emit('decent.core.shape.render', {
       shape: layout.main,
       renderStream: renderer
     });
