2a337e8c86021a3f91c378008bbd269ac4245d7f
Bertrand Le Roy <bertrandleroy@gmail.com>
2014-11-24 00:02:56 -0800

    Title part view not using scope.

--- modules/core/parts/views/title.js
+++ modules/core/parts/views/title.js
@@ -1,7 +1,7 @@
 // DecentCMS (c) 2014 Bertrand Le Roy, under MIT. See LICENSE.txt for licensing details.
 'use strict';
 
-module.exports = function titleTemplate(title, renderer, shell) {
+module.exports = function titleTemplate(title, renderer) {
   renderer.write('<h1>');
   renderer.writeEncoded(title.text);
   renderer.writeLine('</h1>');
