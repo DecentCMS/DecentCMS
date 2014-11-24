f82565c6d6053ba1940bf2615f971541a15dba74
Bertrand Le Roy <bertrandleroy@gmail.com>
2014-11-23 16:28:29 -0800

    Gruntfile added to express module.

---
+++ modules/core/express/Gruntfile.js
@@ -0,0 +1,17 @@
+// DecentCMS (c) 2014 Bertrand Le Roy, under MIT. See LICENSE.txt for licensing details.
+'use strict';
+
+module.exports = function gruntModule(grunt) {
+  grunt.loadNpmTasks('grunt-mocha-test');
+
+  grunt.initConfig({
+    mochaTest: {
+      test: {
+        src: ['test/*.js']
+      }
+    }
+  });
+
+  grunt.registerTask('test', 'mochaTest');
+  grunt.registerTask('default', ['mochaTest']);
+};
