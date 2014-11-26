f901233110f0a3b784dd844ca6b57f95835a301c
Bertrand Le Roy <bertrandleroy@gmail.com>
2014-11-25 19:28:12 -0800

    Rewrite content template to use helper methods on RenderStream.

--- modules/core/ui/views/content.js
+++ modules/core/ui/views/content.js
@@ -1,32 +1,13 @@
 // DecentCMS (c) 2014 Bertrand Le Roy, under MIT. See LICENSE.txt for licensing details.
 'use strict';
 
-function layoutTemplate(content, renderer, scope) {
-  renderer.write('<article>');
-  if (content.header) {
-    renderer.write('<header>');
-    // TODO: move that to a util method on the renderStream.
-    scope.emit('decent.core.shape.render', {
-      shape: content.header,
-      renderStream: renderer
-    });
-    renderer.writeLine('</header>');
-  }
-  if (content.main) {
-    scope.emit('decent.core.shape.render', {
-      shape: content.main,
-      renderStream: renderer
-    });
-  }
-  if (content.footer) {
-    renderer.write('<footer>');
-    scope.emit('decent.core.shape.render', {
-      shape: content.footer,
-      renderStream: renderer
-    });
-    renderer.writeLine('</footer>');
-  }
-  renderer.writeLine('</article>');
+function layoutTemplate(content, renderer) {
+  renderer
+    .startTag('article')
+    .shape(content.header, 'header')
+    .shape(content.main)
+    .shape(content.footer, 'footer')
+    .endTag();
 }
 
 module.exports = layoutTemplate;
