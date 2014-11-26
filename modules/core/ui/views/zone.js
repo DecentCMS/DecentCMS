1e9dd098af42d87cec3fd02b7b7255159c28581c
Bertrand Le Roy <bertrandleroy@gmail.com>
2014-11-25 21:02:50 -0800

    Zone template should use shape helper.

--- modules/core/ui/views/zone.js
+++ modules/core/ui/views/zone.js
@@ -6,21 +6,13 @@ function zoneTemplate(zone, renderer, scope) {
   var items = zone.temp.items;
   if (items && Array.isArray(items)) {
     for (var i = 0; i < items.length; i++) {
-      scope.emit('decent.core.shape.render', {
-        shape: items[i],
-        renderStream: renderer
-      });
-      renderer.writeLine();
+      renderer.shape(items[i]);
     }
   }
   var zones = zone.temp.zones;
   if (zones) {
     for (var zoneName in zones) {
-      scope.emit('decent.core.shape.render', {
-        shape: zones[zoneName],
-        renderStream: renderer
-      });
-      renderer.writeLine();
+      renderer.shape(zones[zoneName]);
     }
   }
 }
