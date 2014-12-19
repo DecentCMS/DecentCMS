4b329f0f85286eff193840753d11ae5bec341545
Bertrand Le Roy <bertrandleroy@gmail.com>
2014-12-18 19:05:24 -0800

    Change Dust extension to .tl

--- modules/core/ui/services/dust-view-engine.js
+++ modules/core/ui/services/dust-view-engine.js
@@ -57,7 +57,7 @@ function getDustTemplate(templatePath) {
 var codeViewEngine = {
   service: 'view-engine',
   feature: 'dust-view-engine',
-  extension: 'dust',
+  extension: 'tl',
   scope: 'shell',
   /**
    * @description
