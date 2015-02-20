1b0fa1171c8c817400a5f849a173084b37877fc0
Bertrand Le Roy <bertrandleroy@gmail.com>
2015-02-20 01:40:51 -0800

    Fix order by doc comment.

--- modules/core/search/services/index.js
+++ modules/core/search/services/index.js
@@ -21,8 +21,9 @@ Index.scope = 'shell';
  *   It is recommended to name the map function.
  * @param {Function} [context.orderBy] The function that defines the order
  *   on which the index entries should be sorted.
- *   It takes two index entries A and B, and returns -1 if A comes before B,
- *   and +1 if A comes after B.
+ *   It takes an index entry, and returns the sort order.
+ *   The sort order can be a simple string, date, number, or it can be an array,
+ *   in which case the items in the array will be used one after the other.
  *   It is recommended to name the order function.
  * @returns {object} The index object.
  */
