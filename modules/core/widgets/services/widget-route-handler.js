14c81357bfd58fb1d48e9fa91745913a730bb7cf
Bertrand Le Roy <bertrandleroy@gmail.com>
2015-02-20 18:17:17 -0800

    Move requires closer to usage.

--- modules/core/widgets/services/widget-route-handler.js
+++ modules/core/widgets/services/widget-route-handler.js
@@ -1,8 +1,5 @@
 // DecentCMS (c) 2014 Bertrand Le Roy, under MIT. See LICENSE.txt for licensing details.
 'use strict';
-var evaluate = require('static-eval');
-var parse = require('esprima').parse;
-
 /**
  * @description
  * A route handler that determines what widgets to render for this request.
@@ -23,32 +20,35 @@ WidgetRouteHandler.prototype.handle = function handleForWidgets(context, next) {
   scope.callService('layer-store', 'loadLayers',
     loadLayerContext, function onLayersLoaded(err) {
 
-    var layers = loadLayerContext.layers;
-    if (!layers) {
-      next();
-      return;
-    }
-    var ruleEvaluationScope = {};
-    scope.callService('layer-rule-context-builder', 'buildContext',
-      ruleEvaluationScope, function onRuleContextBuilt(err) {
+      var layers = loadLayerContext.layers;
+      if (!layers) {
+        next();
+        return;
+      }
+      var ruleEvaluationScope = {};
+      scope.callService('layer-rule-context-builder', 'buildContext',
+        ruleEvaluationScope, function onRuleContextBuilt(err) {
 
-      var renderer = scope.require('renderer');
-      Object.getOwnPropertyNames(layers).forEach(function forEachLayer(layerName) {
-        var layer = layers[layerName];
-        var rule = layer.rule || "true";
-        var ruleAst = ruleAstCache[rule] || (ruleAstCache[rule] = parse(rule).body[0].expression);
-        if (evaluate(ruleAst, ruleEvaluationScope)) {
-          var widgets = layer.widgets;
-          if (widgets) {
-            widgets.forEach(function(widget) {
-              renderer.promiseToRender(widget);
-            });
-          }
-        }
-      });
-      next();
+          var evaluate = require('static-eval');
+          var parse = require('esprima').parse;
+
+          var renderer = scope.require('renderer');
+          Object.getOwnPropertyNames(layers).forEach(function forEachLayer(layerName) {
+            var layer = layers[layerName];
+            var rule = layer.rule || "true";
+            var ruleAst = ruleAstCache[rule] || (ruleAstCache[rule] = parse(rule).body[0].expression);
+            if (evaluate(ruleAst, ruleEvaluationScope)) {
+              var widgets = layer.widgets;
+              if (widgets) {
+                widgets.forEach(function (widget) {
+                  renderer.promiseToRender(widget);
+                });
+              }
+            }
+          });
+          next();
+        });
     });
-  });
 };
 
 module.exports = WidgetRouteHandler;
