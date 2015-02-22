1b3d406ce8824eb84357054612006ffa5a4c10d4
Bertrand Le Roy <bertrandleroy@gmail.com>
2015-02-22 13:49:40 -0800

    Put local TOC under current top-level active entry.

--- modules/core/documentation/views/documentation-toc.js
+++ modules/core/documentation/views/documentation-toc.js
@@ -6,55 +6,77 @@ module.exports = function documentationTocTemplate(toc, renderer, done) {
   var showBreadcrumbs = !!toc.showBreadcrumbs;
   var showNextPrevious = !! toc.showNextPrevious;
   var t = renderer.scope.require('localization');
-  renderer.startTag('nav', {class: 'toc'});
-  if (showNextPrevious) {
-    renderer.startTag('div', {class: 'previous-next'});
-    if (toc.previous) {
-      renderer.tag('a', {href: toc.previous.url, class: 'previous'}, t('&lt; previous'));
-      if (toc.next) {
-        renderer.write(t(' | '));
+  renderer.startTag('nav', {class: 'docs-sidenav'});
+  if (showBreadcrumbs) {
+    renderer.startTag('ol', {class: 'breadcrumb'});
+    toc.breadcrumbs.forEach(function(crumb, index) {
+      var isLast = index === toc.breadcrumbs.length - 1;
+      var cls = isLast ? 'active' : null;
+      var attr = cls ? {class: cls} : null;
+      renderer.startTag('li', attr);
+      if (isLast) {
+        renderer.write(crumb.title);
       }
-    }
-    if (toc.next) {
-      renderer.tag('a', {href: toc.next.url, class: 'next'}, t('next &gt;'));
-    }
+      else {
+        renderer.tag('a', {href: crumb.url}, crumb.title);
+      }
+      renderer.endTag();
+    });
     renderer.endTag();
   }
-  if (showBreadcrumbs) {
-    renderer.startTag('ul', {class: 'breadcrumbs'});
-    toc.breadcrumbs.forEach(function(crumb) {
+  if (showNextPrevious) {
+    renderer.startTag('ul', {class: 'pager'});
+    if (toc.previous) {
       renderer
-        .startTag('li')
-        .tag('a', {href: crumb.url}, crumb.title)
+        .startTag('li', {class: 'previous'})
+        .startTag('a', {href: toc.previous.url})
+        .startTag('span', {class: 'glyphicon glyphicon-chevron-left', 'aria-hidden': 'true'})
+        .endTag()
+        .write('&nbsp;' + t('previous'))
+        .endTag()
         .endTag();
-    });
+    }
+    if (toc.next) {
+      renderer
+        .startTag('li', {class: 'next'})
+        .startTag('a', {href: toc.next.url})
+        .write(t('next') + '&nbsp;')
+        .startTag('span', {class: 'glyphicon glyphicon-chevron-right', 'aria-hidden': 'true'})
+        .endTag()
+        .endTag()
+        .endTag();
+    }
     renderer.endTag();
   }
   if (showTopLevelTOC) {
+    var currentTopLevelItem = toc.breadcrumbs && toc.breadcrumbs[0]
+        ? toc.breadcrumbs[0] : null;
     renderer
       .startTag('ul', {class: 'toc'});
     toc.topLevelTOC.forEach(function(entry) {
+      var isActive = toc.localTOC
+        && currentTopLevelItem
+        && currentTopLevelItem.itemId === entry.itemId;
       renderer
-        .startTag('li')
-        .tag('a', {href: entry.url}, entry.title)
-        .endTag();
+        .startTag('li', isActive ? {class: 'active'} : null)
+        .tag('a', {href: entry.url}, entry.title);
+      if (isActive) {
+        renderer
+          .startTag('ul', {class: 'local-toc toc'});
+        toc.localTOC.forEach(function(entry) {
+          isActive = entry.itemId === (toc.current ? toc.current.itemId : null);
+          renderer
+            .startTag('li', isActive ? {class: 'active'} : null)
+            .tag('a', {href: entry.url}, entry.title)
+            .endTag();
+        });
+        renderer
+          .endTag();
+      }
+      renderer.endTag();
     });
     renderer
       .endTag();
-
-    // TODO: render this under the current module/section
-    if (toc.localTOC) {
-      renderer
-        .startTag('ul', {class: 'local-toc'});
-      toc.localTOC.forEach(function(entry) {
-        renderer
-          .startTag('li')
-          .tag('a', {href: entry.url}, entry.title)
-          .endTag();
-      });
-      renderer
-        .endTag();
-    }
   }
   renderer
     .endTag()
