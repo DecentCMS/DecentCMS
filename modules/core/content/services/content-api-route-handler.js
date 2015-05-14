80ecff431dd99378045923c57d4bc1f95639fef1
Bertrand Le Roy <bertrandleroy@gmail.com>
2015-05-14 11:25:41 -0700

    Make JSON for an item available under /api/src/[id], and the JSON for its processed shapes under /api/shapes/[id]

--- modules/core/content/services/content-api-route-handler.js
+++ modules/core/content/services/content-api-route-handler.js
@@ -13,16 +13,22 @@ var ContentApiRouteHandler = {
   scope: 'shell',
   /**
    * Registers the default content API middleware.
+   * * `/api/src/` in front of a content item id gets its document source.
+   * * `/api/shapes/` in front of a content item is gets the processed shape for it.
    * @param {object} scope The scope.
    * @param {object} context The context.
    * @param {object} context.expressApp The Express application object.
    */
   register: function registerContentApiMiddleware(scope, context) {
     context.expressApp.register(ContentApiRouteHandler.routePriority, function bindContentMiddleware(app) {
-      app.get('/api(/*)?', function contentMiddleware(request, response, next) {
+      app.get('/api/src(/*)?', function contentSrcMiddleware(request, response, next) {
         if (request.routed) {next();return;}
-        var id = request.path === '/api' ? '/' : require('querystring').unescape(request.path.substr(5));
+        var id = request.path === '/api/src'
+          ? '/' : require('querystring').unescape(request.path.substr(9));
         response.type('json');
+        if (request.debug) {
+          request.app.set('json spaces', 2);
+        }
         if (id) {
           var storage = request.require('storage-manager');
           storage.promiseToGet(id);
@@ -30,7 +36,7 @@ var ContentApiRouteHandler = {
             var item = storage.getAvailableItem(id);
             if (item) {
               delete item.temp;
-              response.send(item);
+              response.json(item);
 
               request.routed = true;
               request.handled = true;
@@ -38,7 +44,60 @@ var ContentApiRouteHandler = {
             }
             else {
               response.status(404);
-              response.send({error: 'Not found'});
+              response.json({error: 'Not found'});
+
+              request.routed = true;
+              request.handled = true;
+              next();
+            }
+          });
+        }
+      });
+      app.get('/api/shapes(/*)?', function contentShapeMiddleware(request, response, next) {
+        if (request.routed) {next();return;}
+        var id = request.path === '/api/shapes'
+          ? '/' : require('querystring').unescape(request.path.substr(11));
+        response.type('json');
+        if (request.debug) {
+          request.app.set('json spaces', 2);
+        }
+        if (id) {
+          var storage = request.require('storage-manager');
+          storage.promiseToGet(id);
+          storage.fetchContent({request: request}, function () {
+            var item = storage.getAvailableItem(id);
+            if (item) {
+              var shape = {
+                meta: {type: 'content'},
+                temp: {item: item, shapes: []}
+              };
+              request.callService('shape-handler', 'handle', {
+                  scope: request,
+                  shape: shape
+                }, function renderShapeJson(err) {
+                  if (err) {
+                    response.status(500);
+                    response.json({error: err});
+                    next();
+                    return;
+                  }
+                  // Remove the meta.item and temp.item from each shape before rendering, to avoid circular structures that JSON won't serialize
+                  var shapes = shape.temp.shapes;
+                  shapes.forEach(function removeTempItem(shape) {
+                    delete shape.meta.item;
+                    delete shape.temp.item;
+                  });
+                  // Send the serialized list of shapes.
+                  response.json(shapes);
+
+                  request.routed = true;
+                  request.handled = true;
+                  next();
+                });
+            }
+            else {
+              response.status(404);
+              response.json({error: 'Not found'});
 
               request.routed = true;
               request.handled = true;
