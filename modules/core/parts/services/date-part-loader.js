5c07fc847e5483533d20753d66751d95f05c42d0
Bertrand Le Roy <beleroy@microsoft.com>
2018-10-12 00:30:28 -0400

    Date part

--- modules/core/parts/services/date-part-loader.js
+++ modules/core/parts/services/date-part-loader.js
@@ -3,42 +3,37 @@
 var path = require('path');
 
 /**
- * The date part handler creates date shapes.
+ * The date part loader parses date strings into real date objects.
  */
 var DatePart = {
   feature: 'core-parts',
-  service: 'date-part-handler',
+  service: 'date-part-loader',
   /**
-   * Adds a date shape to `context.shapes` for the date part on the context.
+   * Parses date strings into real dates.
    * @param {object} context The context object.
    * @param {object} context.part The date part to handle.
    * @param {string} context.partName The name of the part.
-   * @param {string} context.displayType The display type.
    * @param {object} context.item A reference to the content item.
-   * @param {Array} context.shapes The shapes array to which new shapes must be pushed.
    * @param {object} context.scope The scope.
    * @param {Function} done The callback.
    */
-  handle: function handleDatePart(context, done) {
-    var shapes = context.shapes;
-    if (!shapes) {done();return;}
-    var part = context.part;
+  load: function loadDatePart(context, done) {
+    var part = context.part.date || context.part;
     var date = part instanceof Date
-      ? part : new Date(Date.parse(part));
+      ? part : typeof(part) === 'string'
+      ? new Date(Date.parse(part)) : null;
     var locale = part.locale || (part.meta ? part.meta.locale : null) || 'en-US';
     var options = part.options || {};
-    shapes.push({
+    context.item[context.partName] = {
       meta: {
         type: 'date',
         name: context.partName,
-        alternates: ['date-' + context.partName],
         item: context.item
       },
-      temp: {displayType: context.displayType},
       locale: locale,
       options: options,
       date: date
-    });
+    };
     done();
   }
 };
